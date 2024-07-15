320b3828f673de8c635bfe0a1e03287875094f94
soraya1618 <sorayyakhawari@gmail.com>
2024-07-15 11:42:39 +0200

    05/ getInstance

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,11 +3,20 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
+Cat cat = new Cat();
+output("Blick von aussen" + cat);
+output("Blick von aussen" + cat.getInstanceVariable());
 
+System.out.println("---------------");
 
+Cat cat2 = new Cat();
+output("Blick von aussen" + cat2);
+output("Blick von aussen" + cat2.getInstanceVariable());
     }
 
-   
+   public static void output(String outputStr){
+    System.out.println(outputStr);
+   } 
 
 }
 
---
+++ com/cc/java/Cat.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class Cat {
+    
+    //this -- Instanzrariable
+//     void tellyourAddress(){
+//        App.output("Blick von aussen" + this);
+//     }
+
+// }
+  
+    public Cat getInstanceVariable(){
+        return this;
+    }
